8b0c13be4f1b92d9c398af7bbf14d9e5ae7728fc
Alan Dean <adean3032@gmail.com>
2023-01-08 21:37:17 -0500

    Adds custom error handler

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const morgan = require("morgan")
-const postBank  = require("./postBank")
-
+const morgan = require("morgan");
+const postBank = require("./postBank");
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
@@ -21,51 +20,31 @@ app.get("/", (req, res) => {
   <body>
     <div class="news-list">
       <header><img src="/logo.png"/>Wizard News</header>
-      ${posts.map(post => `
+      ${posts
+        .map(
+          (post) => `
         <div class='news-item'>
-          <p>
+          <a href='/posts/${post.id}'>
             <span class="news-position">${post.id}. ▲</span>
             <a href="/posts/${post.id}">${post.title}</a>
             <small>(by ${post.name})</small>
-          </p>
+          </a>
           <small class="news-info">
             ${post.upvotes} upvotes | ${post.date}
           </small>
         </div>`
-      ).join('')}
+        )
+        .join("")}
     </div>
   </body>
 </html>`;
 
-
-
-  res.send(html)
+  res.send(html);
 });
 
-app.get('/posts/:id', (req, res) => {
+app.get("/posts/:id", (req, res) => {
   const id = req.params.id;
   const post = postBank.find(id);
-
-  if (!post.id) {
-    // If the post wasn't found, set the HTTP status to 404 and send Not Found HTML
-    res.status(404)
-    const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-      <header><img src="/logo.png"/>Wizard News</header>
-      <div class="not-found">
-        <p>Accio Page! 🧙‍♀️ ... Page Not Found</p>
-        <img src="/dumbledore-404.gif" />
-      </div>
-    </body>
-    </html>`
-    res.send(html)
-  } else {
 
   const html = `<!DOCTYPE html>
   <html>
@@ -90,13 +69,36 @@ app.get('/posts/:id', (req, res) => {
   </body>
 </html>`;
 
+if (!post.id) {
+  res.status(404)
+  throw Error;
+}
+
   res.send(html);
-}})
+});
 
+app.use((err, req, res, next) => {
+  if (res.statusCode === 404) {
+    const notFoundHTML = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Wizard News</title>
+          <link rel="stylesheet" href="/style.css" />
+        </head>
+        <body>
+          <header><img src="/logo.png"/>Wizard News</header>
+          <div class="not-found">
+            <p>Accio Page! 🧙‍♀️ ... Page Not Found (404)</p>
+          </div>
+        </body>
+      </html>`;
+    res.send(notFoundHTML);
+  }
+}
+);
 
-const { PORT = 1337 } = process.env
-
+const { PORT = 1337 } = process.env;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
-
